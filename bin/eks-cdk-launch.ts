98886f828e312b897b2bcf3e4ef6d176d5afbc45
entest <hai@entst.io>
2023-04-25 04:03:45 +0000

    first

--- bin/eks-cdk-launch.ts
+++ bin/eks-cdk-launch.ts
@@ -1,21 +1,26 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { EksCdkLaunchStack } from '../lib/eks-cdk-launch-stack';
+import * as cdk from "aws-cdk-lib";
+import { VpcStack } from "../lib/network-stack";
+import { EksClusterStack } from "../lib/eks-cluster-level1-stack";
 
 const app = new cdk.App();
-new EksCdkLaunchStack(app, 'EksCdkLaunchStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const network = new VpcStack(app, "NetworkStack", {
+  cidr: "10.0.0.0/16",
+  name: "Network",
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
 });
+
+const eks = new EksClusterStack(app, "CdkEksFargateStack", {
+  clusterName: "EksClusterLevel2",
+  vpc: network.vpc,
+  eksSecurityGroup: network.eksSecurityGroup,
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
+
+eks.addDependency(network);
