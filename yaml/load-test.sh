3704bba71e834032cd922a8e37a7e2f0f3b5a8ae
entest <hai@entst.io>
2023-05-04 05:15:41 +0000

    add load test

---
+++ yaml/load-test.sh
@@ -0,0 +1,4 @@
+artillery quick --num 10000 --count 500 "http://aab7e1494e3cc4a20ac3f441d9cc97d1-1153310976.ap-southeast-1.elb.amazonaws.com"
+#kubect get hpa --watch 
+#kubect top pod -n default 
+#kubect top node 
