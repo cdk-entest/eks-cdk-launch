da3e161bd64fa4a55ffd2c86d896abab30f02002
entest <hai@entst.io>
2023-06-29 07:22:17 +0000

    update

---
+++ next-app/build.py
@@ -0,0 +1,26 @@
+import os 
+
+# parameters
+REGION = "ap-southeast-1"
+ACCOUNT = "227135398356"
+
+# delete all docker images 
+os.system("sudo docker system prune -a") 
+
+# build next-app image 
+os.system("sudo docker build -t next-app . ")
+
+#  aws ecr login 
+os.system(f"aws ecr get-login-password --region {REGION} | sudo docker login --username AWS --password-stdin {ACCOUNT}.dkr.ecr.{REGION}.amazonaws.com")
+
+# get image id 
+IMAGE_ID=os.popen("sudo docker images -q next-app:latest").read()
+
+# tag next-app image 
+os.system(f"sudo docker tag {IMAGE_ID.strip()} {ACCOUNT}.dkr.ecr.{REGION}.amazonaws.com/next-app:latest")
+
+# create ecr repository 
+os.system(f"aws ecr create-repository --registry-id {ACCOUNT} --repository-name next-app")
+
+# push image to ecr 
+os.system(f"sudo docker push {ACCOUNT}.dkr.ecr.{REGION}.amazonaws.com/next-app:latest")
