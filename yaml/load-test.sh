c0b7e1503aea739d6104c8877b66acab6839c880
entest <entest>
2023-08-14 06:43:01 +0700

    test

--- yaml/load-test.sh
+++ yaml/load-test.sh
@@ -1,4 +1,6 @@
-artillery quick --num 10000 --count 500 "http://aab7e1494e3cc4a20ac3f441d9cc97d1-1153310976.ap-southeast-1.elb.amazonaws.com"
-#kubect get hpa --watch 
-#kubect top pod -n default 
-#kubect top node 
+artillery quick --num 10000 --count 500 "https:/kube.entest.io"
+# kubectl get hpa book-app-hpa --watch 
+# kubectl top pod -n default 
+# kubectl top node 
+# kubectl -n kube-system logs -f deployment.apps/cluster-autoscaler
+# kubectl rollout restart deployment/flask-app-deployment
